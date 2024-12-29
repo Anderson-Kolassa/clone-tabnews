ab44eb9d24b3ef4dd58b7ddbdda211c7feba3386
Anderson Kolassa <andersonotelakoski@gmail.com>
2024-12-29 04:15:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do TabNews para se tornar competente em programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home (){
-    return <h1>Camila meu amor, eu te amo demais e se tu me ama, da uma risadinha! </h1>
+function Home() {
+  return (
+    <h1>
+      Camila meu amor, eu te amo demais e se tu me ama, da uma risadinha!{" "}
+    </h1>
+  );
 }
 
 export default Home;
